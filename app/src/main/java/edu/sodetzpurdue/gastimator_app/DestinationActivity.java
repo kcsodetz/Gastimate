3ed0418b9593b2d01c1656c23f3c91ce2c348122
Stefany Go <gunawang@purdue.edu>
2017-10-01 04:22:55 -0400

    DestinationActivity
    Add Input Manipulations

--- app/src/main/java/edu/sodetzpurdue/gastimator_app/DestinationActivity.java
+++ app/src/main/java/edu/sodetzpurdue/gastimator_app/DestinationActivity.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class DestinationActivity extends AppCompatActivity {
     private EditText origin, destination;
     Button gastimate;
-    String originString, destinationString;
+    String originString = "Origin";
+    String destinationString = "Destination";
+    String response = "empty";
+    public final int NO_ORIGIN = 0;
+    public final int NO_DESTINATION = 1;
+    public final int SUCCESS = 2;
+    public final int DEFAULT = 3;
+    GetDistance getDistance = new GetDistance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +30,44 @@ public class DestinationActivity extends AppCompatActivity {
                 destination = (EditText)findViewById(R.id.editText2);
                 originString = origin.getText().toString();
                 destinationString = destination.getText().toString();
-                
+                if(originString.equals("")) {
+                    messageToast(NO_ORIGIN);
+                }
+                else if(destinationString.equals("")) {
+                    messageToast(NO_DESTINATION);
+                }
+                if(originString != null && destinationString != null &&
+                        !originString.equalsIgnoreCase("make") && !destinationString.equalsIgnoreCase("model")) {
+                    response = getDistance.googleMapsConnect(originString, destinationString);
+                }
+
+
             }
         });
+    }
+
+    /**
+     * Handles toasts for missing inputs
+     * @param input missing input
+     */
+    public void messageToast(int input){
+        switch (input){
+            case NO_ORIGIN:
+                Toast.makeText(this, "You did not enter an Origin", Toast.LENGTH_SHORT).show();
+                break;
+            case NO_DESTINATION:
+                Toast.makeText(this, "You did not enter a Destination", Toast.LENGTH_SHORT).show();
+                break;
+            case SUCCESS:
+                Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+                break;
+            case DEFAULT:
+                Toast.makeText(this, "Please input values", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
 }
